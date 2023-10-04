269fe7a3053cd330628f273a6b1d4011fc4e6df5
Ashkan Hashemi <ashkanhsm2000@gmail.com>
2023-10-04 15:48:09 +0330

    Add files via upload

---
+++ Digital System Design Lab 5-1/Source/My_Functions.h
@@ -0,0 +1,19 @@
+#ifndef _My_Functions_h_INCLUDED_
+#define _My_Functions_h_INCLUDED_
+//Q1=====
+/*
+void First_Initialazeation();
+
+*/
+void Q2_Function(int DutyCycle);
+
+void Q4_Function();
+void Q5_function_TwoPhase();
+void TurnOFF_AllTimers();
+void turnOn_SelectedTimer(int SelectTimer);
+
+
+
+
+#endif
+
